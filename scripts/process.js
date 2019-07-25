291aad7765bf3698e9d92d560d2eb7dbbe2b4f79
Eduardo Kraus <kraus@eduardokraus.com>
2019-07-25 15:24:16 -0300

    Format

--- scripts/process.js
+++ scripts/process.js
@@ -36,24 +36,20 @@ function proccessReplace ( URL_VIDEOTECA ) {
             URL_VIDEOTECA );
         replaceUrl ( "platforms/ios/www/plugins/cordova-plugin-videoteca/www/Videoteca.js",
             URL_VIDEOTECA );
-    }
-    if ( directoryExists ( "platforms/android" ) ) {
-
+    } else if ( directoryExists ( "platforms/android" ) ) {
         // Cordova Android 7
         if ( directoryExists ( "platforms/android/app/src/main/assets/www/plugins/cordova-plugin-videoteca/www" ) ) {
             replaceUrl ( "platforms/android/app/src/main/assets/www/plugins/cordova-plugin-videoteca/www/Videoteca.js",
                 URL_VIDEOTECA );
             replaceUrl ( "platforms/android/app/src/main/assets/www/plugins/cordova-plugin-videoteca/www/Videoteca.js",
                 URL_VIDEOTECA );
-        }else {
-
+        } else {
             replaceUrl ( "platforms/android/platform_www/plugins/cordova-plugin-videoteca/www/Videoteca.js",
                 URL_VIDEOTECA );
             replaceUrl ( "platforms/android/assets/www/plugins/cordova-plugin-videoteca/www/Videoteca.js",
                 URL_VIDEOTECA );
         }
-    }
-    if ( directoryExists ( "platforms/browser" ) ) {
+    } else if ( directoryExists ( "platforms/browser" ) ) {
         replaceUrl ( "platforms/browser/www/plugins/cordova-plugin-videoteca/www/Videoteca.js",
             URL_VIDEOTECA );
     }
